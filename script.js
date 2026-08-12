10675876246d41f55bb8e56faec82d4d40f5d7fa
Tiputo <parekhonza@gmail.com>
2026-08-12 13:41:00 +0200

    Issued the budget value keeps the text content value after refreshing the website

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 /* TO-DO: 
 PRIORITA
-- Storing data z lokalu = udelat tak, aby ty hodnoty tam porad zustali
-- Pak kdyz se to bude ukladat, tak udelat refresh button na to, aby se to vsechno tam smazalo
-- Udelat, ze nemuzu jit do minusu s hodnotami 
+1) Storing data z lokalu = udelat tak, aby ty hodnoty tam porad zustali
+^^^Funguje pro muj budget zatim (udelal bych pak hodne velkou podminku pro to vevnitr formu)
+
+2) Pak kdyz se to bude ukladat, tak udelat refresh button na to, aby se to vsechno tam smazalo
+3) Udelat, ze nemuzu jit do minusu s hodnotami myho budgetu 
 
 MENSI PRIORITA
 az dodelany, tak to udelat prehledneji ten kod
@@ -11,8 +13,8 @@ search engine, kde muzu vyhledat produkt, rozkliknout si, co chci a pridat to do
 */
 
 const myBudget = document.querySelector(".budgetSubmit");
-const myItem = document.querySelector(".needSubmit");
 const myNumItem = document.querySelector(".needNumSubmit");
+const myItem = document.querySelector(".needSubmit");
 
 const card = document.querySelector(".card");
 const remaining = document.querySelector(".remaining");
@@ -38,41 +40,40 @@ firstForm.forEach((form) => {
     const myBudgetValue = myBudget.value;
     addTextBudget.textContent = "Můj budget: " + myBudgetValue + " kč";
     sessionStorage.setItem("myBudget", myBudgetValue);
-    sessionStorage.getItem("myBudget");
     card.prepend(addTextBudget);
-    getBudget = totalBudget(parseInt(myBudget.value));
+    getBudget = totalBudget(parseInt(myBudgetValue));
   });
 });
+loadForm();
 
 const secondForm = document.querySelectorAll(".submit1");
 secondForm.forEach((form) => {
   form.addEventListener("submit", (event) => {
     event.preventDefault();
+
     const addItem = document.createElement("p");
-    const itemValue = myItem.value;
     let numItemValue = parseInt(myNumItem.value);
+    const itemValue = myItem.value;
 
     addItem.textContent = itemValue + `: ${numItemValue} kč`;
-
     sessionStorage.setItem(itemValue, numItemValue);
-    sessionStorage.getItem(itemValue);
 
     removeItem(addItem, getBudget, itemValue, numItemValue);
     getBudget.deductedTotalBudget(numItemValue);
   });
 });
 
-function removeItem(addItem, getBudget, itemValue, numItemValue){
+function removeItem(addItem, getBudget, itemValue, numItemValue) {
   const getButton = document.getElementById(".buttonItem");
-    let button = document.createElement("button");
-    button.innerText = "Smazat";
+  let button = document.createElement("button");
+  button.innerText = "Smazat";
   button.addEventListener("click", () => {
-      button.remove();
-      addItem.remove();
-      getBudget.refundTotalBudget(numItemValue);
-      sessionStorage.removeItem(itemValue);
-    });
-    remaining.prepend(addItem, button);
+    button.remove();
+    addItem.remove();
+    getBudget.refundTotalBudget(numItemValue);
+    sessionStorage.removeItem(itemValue);
+  });
+  remaining.prepend(addItem, button);
 }
 
 function totalBudget(budget) {
@@ -81,9 +82,7 @@ function totalBudget(budget) {
   function checkBelow() {
     if (budget < 0) {
       return (remainingBelow.textContent = `Jsi pod nulou!`);
-    }
-    else  
-      return (remainingBelow.textContent = "");
+    } else return (remainingBelow.textContent = "");
   }
 
   function deductedTotalBudget(item) {
@@ -101,4 +100,22 @@ function totalBudget(budget) {
   }
 
   return { deductedTotalBudget, refundTotalBudget };
+}
+
+function loadForm() {
+  let storedBudget = sessionStorage.getItem("myBudget");
+  // console.log(storedBudget);
+
+  window.addEventListener("load", () => {
+    if (storedBudget) {
+      addTextBudget.textContent = `Můj budget: ${storedBudget} kč (ulozeny)`;
+    } else {
+      addTextBudget.textContent = "";
+    }
+    card.prepend(addTextBudget);
+  });
+
+  //  let storedItem = sessionStorage.getItem(myNumItem);
+
+  // let storedBudgetLeft = sessionStorage.getItem("BudgetLeft");
 }
